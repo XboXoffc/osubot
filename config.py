ab5fdd2ba1850f0a88e0119638d8fd5f052367fb
xboxoffc <adel14xbox@google.com>
2025-04-10 22:11:40 +0300

    update config

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 TG_TOKEN = ""
 
 WHEATHER_API = ""
-WHEATHER_DB_PATH = "osubot/data/wheather.db"
+WHEATHER_DB_PATH = 'data/weather.db'
 
 OSU_CLIENT_ID = 
 OSU_CLIENT_SECRET = ''
 X_API_VERSION = '20241130'
-OSU_USERS_DB = 'osubot/data/osu.db'
-OSU_SKIN_PATH = 'osubot/data/skins/'
+OSU_USERS_DB = 'data/osu.db'
+OSU_SKIN_PATH = 'data/skins/'
+OSU_GROUPS_DB = 'data/groups.db'
