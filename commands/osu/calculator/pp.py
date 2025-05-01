eb11d7595fc9c901aafb72d16ca79ba54c5c8837
xboxoffc <adel14xbox@google.com>
2025-05-01 13:27:28 +0300

    fix

--- commands/osu/calculator/pp.py
+++ commands/osu/calculator/pp.py
@@ -44,8 +44,6 @@ async def main(map_id, mode=None, mods=None, lazer=None, clockrate=None,
             accuracy = 100,
             n300 = n300 + n100 + n50 + misses
         ).calculate(beatmap)
-        print(attrs_ss)
-        print(mods)
 
         attrs_99 = rosu.Performance(
             mods = mods,
