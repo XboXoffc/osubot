a92095eba8f150f5de7d4c42729b58349e3a831b
xboxoffc <adel14xbox@google.com>
2025-06-13 14:49:02 +0300

    update

--- commands/osu/osuapistorage.py
+++
@@ -1,8 +0,0 @@
-from commands.osu import osuapi
-import config
-
-OSU_ID = config.OSU_CLIENT_ID
-OSU_SECRET = config.OSU_CLIENT_SECRET
-X_API_VERSION = config.X_API_VERSION
-
-osu_api = osuapi.Osu(OSU_ID, OSU_SECRET, X_API_VERSION)
